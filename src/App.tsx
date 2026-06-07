a732e541d47a47e01d08010b3986a01d9cc4ac07
Ben Benjamin <weresponddispatch@gmail.com>
2026-06-07 16:42:00 +0400

    Multi-page site final

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import Landing from "./pages/Landing";
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import FeaturesPage from "./pages/FeaturesPage";
+import WhoPage from "./pages/WhoPage";
+import RoadmapPage from "./pages/RoadmapPage";
+import AboutPage from "./pages/AboutPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -17,14 +21,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/features" element={<FeaturesPage />} />
+        <Route path="/who" element={<WhoPage />} />
+        <Route path="/roadmap" element={<RoadmapPage />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
         <Route path="/fx" element={<PrivateRoute><FxQuote /></PrivateRoute>} />
         <Route path="/transfer" element={<PrivateRoute><Transfer /></PrivateRoute>} />
         <Route path="/deposit" element={<PrivateRoute><Deposit /></PrivateRoute>} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
